87c13517c6ae4098bc4e4bdf3c9c94220b47c420
Arya Arun <arya2001bhosale@gmail.com>
2021-05-16 15:19:56 +0530

    Connected firestore db to postsgit status Now all the data on db is live loaded on posts
    ok
    exit
    git status

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import React,{ useState } from 'react';
+import React,{ useState, useEffect } from 'react';
 import './App.css';
 import Post from "./Post";
-
+import { db } from "./firebase"
 function App() {
 
   const [posts,setPosts] = useState([
-    {
-      username:"CarryMinati",
-      caption:"Kaise hai app log",
-      imageUrl:"https://www.iwmbuzz.com/wp-content/uploads/2019/09/carryminati-completes-8-million-youtube-subscribers.jpg"
-    },
-    {
-      username:"Sarthak",
-      caption:"Yoo",
-      imageUrl:"https://resize.indiatvnews.com/en/resize/newbucket/715_-/2016/05/tanmaybhat-1464599791.jpg"
-    },
-    {
-      username:"Arya",
-      caption:"Apkey yahan be aisa hota hai",
-      imageUrl:"https://images.inuth.com/2018/11/feat-img-Gursimran-Khamba-final-Amazon.jpg"
-    },
-    {
-      username:"Prateek",
-      caption:"Mzaa anaa chaiye",
-      imageUrl:"https://th.bing.com/th/id/OIP.3KUHFygi4z6zD6eq2e4IzwHaE8?pid=ImgDet&rs=1"
-    }
+    
   ])
+  useEffect(()=>{
+db.collection('posts').onSnapshot(snapshot =>{
+  setPosts(snapshot.docs.map(doc=>doc.data()));
+})
+  },[]);
   return (
     <div className="app">
      
@@ -37,14 +23,12 @@ function App() {
 
        
      </div>
-     <Post username="CarryMinati:" caption="Kaise hai app log" imageUrl="https://www.iwmbuzz.com/wp-content/uploads/2019/09/carryminati-completes-8-million-youtube-subscribers.jpg"/>
-     <Post username="Sarthak:" caption="Yoo" imageUrl="https://resize.indiatvnews.com/en/resize/newbucket/715_-/2016/05/tanmaybhat-1464599791.jpg"/>
-     <Post username="Arya" caption="Tumhare yahan be aisa hota hai" imageUrl="https://images.inuth.com/2018/11/feat-img-Gursimran-Khamba-final-Amazon.jpg"/>
-     <Post username="Prateek:" caption="Mzaa anaa chaiye" imageUrl="https://th.bing.com/th/id/OIP.3KUHFygi4z6zD6eq2e4IzwHaE8?pid=ImgDet&rs=1"/>
+     
+    
      <h1>Hello</h1>
      {
-       posts.map(posts =>(
-         <posts username = {posts.username} caption = {posts.caption} imageUrl={posts.imageUrl}/>
+       posts.map(post =>(
+         <Post username = {post.username} caption = {post.caption} imageUrl={post.imageUrl}/>
        ))
      }
 
