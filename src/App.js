b6f66063e99aeb666b042e57418ba3a724862fe7
Sarthak Kumar Singh <sarthaksingh0608@gmail.com>
2021-10-17 15:15:15 +0530

    Added classes in app.js

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ function App() {
       {user?.displayName ? (
         <ImageUpload username={user.displayName} />
       ) : (
-        <h3></h3>
+        <h3 class="header">Sorry you need to login again</h3>
       )}
 
       <div className="app__header">
